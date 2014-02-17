3087f3ca147d69fed5c77ead2d34d3e473acd5b3
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-17 16:04:11 +0000

    Holding site

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ILW Smart Data Hackathon 2014</title>
+		<link rel="stylesheet" type="text/css" href="/style.css">
+	</head>
+	<body>
+		<a onclick="javascript:window.open('http://www.youtube.com/watch?v=wyz_2DEah4o', '_blank');">Cue the music</a>
+		<h1>In 2014 our crack developer unit has been sent to Appleton Tower to create a project to commit. Today, still searching for a project, we survive as coders of fortune. If you have a problem, if no one else can help, and if you have pizza, then you can hire the AT Team.</h1>
+	</body>
+</html>
