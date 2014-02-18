96ed552c947d5fdd43425bc865c15c2082cc39ca
Gram <gram@c000090.wlan.net.ed.ac.uk>
2014-02-18 13:26:24 +0000

    basic interface

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html>
+<html lang="en" dir="ltr">
 <html>
 	<head>
-		<title>ILW Smart Data Hackathon 2014</title>
-		<link rel="stylesheet" type="text/css" href="/style.css">
+	<meta charset="UTF-8">
+	<title>ILW Smart Data Hackathon 2014</title>
+
+	<!--Script-->
+    <script type="text/javascript" src="jquery/jquery.js"></script>
+	<script type="text/javaScript" src="mootools/mootools-core-1.4.5-full-compat-yc.js"></script>
+    <script type="text/javaScript" src="script/main.js"></script>
+
+    <!--CSS-->
+    <link media="all" rel="stylesheet" type="text/css" href="css/style.css">
+
+
 	</head>
 	<body>
 		<a onclick="javascript:window.open('http://www.youtube.com/watch?v=wyz_2DEah4o', '_blank');">Cue the music</a>
