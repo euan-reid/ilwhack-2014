6e279f17bf7d3b0406a36211073150d598b194f3
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-18 14:30:36 +0000

    Screw the PHP, I have Javascript

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<?php
-	session_start();
-
-	require_once "../src/apiClient.php";
-	require_once "../src/contrib/apiCalendarService.php";
-
-	$apiClient = new apiClient();
-	$apiClient->setUseObjects(true);
-	$service = new apiCalendarService($apiClient);
-
-	if (isset($_SESSION['oauth_access_token'])) {
-		$apiClient->setAccessToken($_SESSION['oauth_access_token']);
-	} else {
-		$token = $apiClient->authenticate();
-		$_SESSION['oauth_access_token'] = $token;
-	}
-?>
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 	<head>
