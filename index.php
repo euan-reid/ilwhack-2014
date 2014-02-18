798278fdf52ccd9cbd53227cd356957983636db5
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-18 14:18:26 +0000

    Added api client auth

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
+<?php
+	session_start();
+
+	require_once "../src/apiClient.php";
+	require_once "../src/contrib/apiCalendarService.php";
+
+	$apiClient = new apiClient();
+	$apiClient->setUseObjects(true);
+	$service = new apiCalendarService($apiClient);
+
+	if (isset($_SESSION['oauth_access_token'])) {
+		$apiClient->setAccessToken($_SESSION['oauth_access_token']);
+	} else {
+		$token = $apiClient->authenticate();
+		$_SESSION['oauth_access_token'] = $token;
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>ILW Smart Data Hackathon 2014</title>
+		<title>LifeCal</title>
 		<link rel="stylesheet" type="text/css" href="/style.css">
 	</head>
 	<body>
-		<a onclick="javascript:window.open('http://www.youtube.com/watch?v=wyz_2DEah4o', '_blank');">Cue the music</a>
-		<h1>In 2014 our crack developer unit has been sent to Appleton Tower to create a project to commit. Today, still searching for a project, we survive as coders of fortune. If you have a problem, if no one else can help, and if you have pizza, then you can hire the AT Team.</h1>
+		
 	</body>
 </html>
