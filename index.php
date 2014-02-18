3f6a6206c5dc5ac51dd4cb7b77a7248da162867b
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-18 14:40:16 +0000

    Added Google Data config

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 		<!--Script-->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed.js"></script>
+		<script src="https://apis.google.com/js/client.js?onload=OnLoadCallback"></script>
+		<script src="/script/gconf.js"></script>
 		<script type="text/javascript" src="/script/main.js"></script>
 
 		<!--CSS-->
---
+++ script/gconf.js
@@ -0,0 +1,3 @@
+var clientId = '182653051318';
+var apiKey = 'AIzaSyCojj5ZHvVx1njVabv5TfE1zETZlAKaQTA';
+var scopes = 'https://www.googleapis.com/auth/calendar';
