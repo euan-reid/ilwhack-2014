8523f5036b989ac05da150801ba7adc5b82d026d
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-18 14:52:26 +0000

    Fixed google data onload call

--- script/gconf.js
+++ script/gconf.js
@@ -1,3 +1,28 @@
 var clientId = '182653051318';
 var apiKey = 'AIzaSyCojj5ZHvVx1njVabv5TfE1zETZlAKaQTA';
 var scopes = 'https://www.googleapis.com/auth/calendar';
+
+function handleClientLoad() {
+	gapi.client.setApiKey(apiKey);
+	window.setTimeout(checkAuth,1);
+}
+
+function checkAuth() {
+	gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+}
+
+function handleAuthResult(authResult) {
+	var authorizeButton = document.getElementById('authorize-button');
+	if (authResult && !authResult.error) {
+		authorizeButton.style.visibility = 'hidden';
+		makeApiCall();
+	} else {
+		authorizeButton.style.visibility = '';
+		authorizeButton.onclick = handleAuthClick;
+	}
+}
+
+function handleAuthClick(event) {
+	gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+	return false;
+}
