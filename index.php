115627c5c4f03cd4ce3a2033b552a9bae73f3485
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-19 10:31:08 +0000

    Gradient meant for entire page

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 	</head>
 	<body>
 		<div class='head'>
-		<img id="banner" src="/imagery/LifeCal-logo.png"/>
+			<img id="banner" src="/imagery/LifeCal-logo.png"/>
 		</div>
 
 		<div id="calendar"></div>
