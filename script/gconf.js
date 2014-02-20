21c5605c1e0fcdb246db3fe315eb76dc285ebeec
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-20 23:50:45 +0000

    Trying to increase response time

--- script/gconf.js
+++ script/gconf.js
@@ -24,7 +24,7 @@ function handleAuthResult(authResult) {
 	if (authResult && !authResult.error) {
 		setInterval(loadCalendarIds, 60000);
 		loadCalendarIds();
-		setTimeout($.proxy(Main.fetchRemoteCalendarEvents, this), 2000);
+		Main.fetchRemoteCalendarEvents();
 	} else {
 		authorizeButton.onclick = handleAuthClick;
 	}
