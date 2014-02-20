662cf6e2475312f6d7a5bb417c91d07ee29419d9
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-20 23:03:10 +0000

    Fixing scope error pt 2

--- script/gconf.js
+++ script/gconf.js
@@ -24,7 +24,7 @@ function handleAuthResult(authResult) {
 	if (authResult && !authResult.error) {
 		setInterval(loadCalendarIds, 60000);
 		loadCalendarIds();
-		setTimeout(fetchRemoteCalendarEvents, 1000).bind(Main);
+		setTimeout(Main.fetchRemoteCalendarEvents, 1000).bind(this);
 	} else {
 		authorizeButton.onclick = handleAuthClick;
 	}
