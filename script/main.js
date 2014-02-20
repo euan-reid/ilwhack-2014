9c0620835c61583d3c69779c80d98c26731c7835
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-20 18:11:36 +0000

    Javascript can't count brackets

--- script/main.js
+++ script/main.js
@@ -43,7 +43,6 @@ var MainClass = new Class({
 			+ (offset > 0 ? "-" : "+")
 			+ this.pad(Math.floor(Math.abs(offset) / 60), 2)
 			+ ":" + this.pad(Math.abs(offset) % 60, 2);
-		}
 	},
 
 	makeEventReal: function(event){
