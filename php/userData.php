060ccd450de829b0d41a5a29a0d63f3e33e9a247
Gram <marek.strelec@gmail.com>
2014-02-20 11:28:49 +0000

    array fix

--- php/userData.php
+++ php/userData.php
@@ -6,21 +6,21 @@ $events = array();
 	$eventsArray['title'] = "Breakfast at Tiffany's";
     $eventsArray['start'] = "2014-02-20 08:00:00";
     $eventsArray['end'] = "2014-02-20 10:00:00";
-    $eventsArray['location'] = [55.953036,-3.191765];
+    $eventsArray['location'] = array(55.953036,-3.191765);
     $events[] = $eventsArray;
 
     $eventsArray = array();
     $eventsArray['title'] = "Lunch with Bob";
     $eventsArray['start'] = "2014-02-20 12:00:00";
     $eventsArray['end'] = "2014-02-20 13:30:00";
-    $eventsArray['location'] = [55.948819,-3.187216];
+    $eventsArray['location'] = array(55.948819,-3.187216);
     $events[] = $eventsArray;
 
     $eventsArray = array();
     $eventsArray['title'] = "Gym - pleasance";
     $eventsArray['start'] = "2014-02-20 15:00:00";
     $eventsArray['end'] = "2014-02-20 18:00:00";
-    $eventsArray['location'] = [55.946764,-3.181315];
+    $eventsArray['location'] = array(55.946764,-3.181315);
     $events[] = $eventsArray;
 
     foreach($events as &$event){
