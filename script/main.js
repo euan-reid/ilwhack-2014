476210d4616d4eb784c5b8c063951cc6b05a65e2
Euan Reid <s0932205@sms.ed.ac.uk>
2014-02-21 11:18:06 +0000

    Possible fix for needing to switch tabs to display events

--- script/main.js
+++ script/main.js
@@ -105,6 +105,7 @@ var MainClass = new Class({
 
 								importData.push(eventData);
 							}
+							Main.showCalendar('#calendar', importData);
 						} else {
 							console.log("Failed to retrieve events from " + key);
 							console.log(resp);
@@ -114,8 +115,6 @@ var MainClass = new Class({
 				}
 				request.execute(callbackMaker(key));
 			}
-
-			Main.showCalendar('#calendar', importData);
 		});
 	},
 
@@ -174,26 +173,26 @@ var MainClass = new Class({
 		var targetLoc = new google.maps.LatLng(location.getX(), location.getY());
 		
 		map = new google.maps.Map(document.getElementById('map-canvas'), {
-	    	center: targetLoc,
-		    zoom: 15
+		center: targetLoc,
+		zoom: 15
 		});
 
 		var request = {
-		  reference: reference
+			reference: reference
 		};
 
 		var infowindow = new google.maps.InfoWindow();
 		service = new google.maps.places.PlacesService(map);
 		service.getDetails(request, function(place, status) {
 			if (status == google.maps.places.PlacesServiceStatus.OK) {
-			  var marker = new google.maps.Marker({
-			    map: map,
-			    position: place.geometry.location
-			  });
-			  google.maps.event.addListener(marker, 'click', function() {
-			    infowindow.setContent(place.name);
-			    infowindow.open(map, this);
-			  });
+				var marker = new google.maps.Marker({
+					map: map,
+					position: place.geometry.location
+				});
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(place.name);
+					sinfowindow.open(map, this);
+				});
 			}
 		});
 
@@ -201,7 +200,7 @@ var MainClass = new Class({
 
 	showMapWithLocationCallbcak: function(place, status){
 		if (status == google.maps.places.PlacesServiceStatus.OK) {
-		    createMarker(place);
+			createMarker(place);
 		}
 	},
 
